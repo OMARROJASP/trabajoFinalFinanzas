2f5308ae75a0ae1fbb474964f17c45d7b3b92011
Miguel Guevara <miguel_g1789@hotmail.com>
2023-06-21 01:05:21 -0500

    styling: :lipstick: develop3 new style in login

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,52 +1,64 @@
 <template>
-  <div class="h-screen flex items-center justify-center bg-blue-600">
-    <div class="bg-white p-10 rounded-xl flex flex-col gap-5">
-      <h1 class="text-4xl font-bold">Nombre del proyecto</h1>
-      <form
-        @submit.prevent="handleSubmit"
-        class="flex flex-col items-center gap-3"
+  <div
+    class="h-screen flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500"
+  >
+    <form
+      @submit.prevent="handleSubmit"
+      class="flex flex-col justify-center items-center h-96 p-10 rounded-xl gap-5 w-4/12 bg-[#ffffff8c] relative"
+    >
+      <div>
+        <img
+          class="absolute -top-[80px] right-[38%]"
+          width="150"
+          src="../assets/logo.svg"
+          alt=""
+        />
+      </div>
+
+      <div class="flex items-center">
+        <div class="p-[13px] bg-white rounded-l-lg">
+          <span class="text-blue-900 material-symbols-outlined">
+            account_circle
+          </span>
+        </div>
+        <input
+          class="p-4 rounded-r-lg bg-blue-900 text-white"
+          type="email"
+          placeholder="Enter your email"
+          v-model.trim="email"
+        />
+      </div>
+
+      <div class="flex items-center">
+        <div class="p-[13px] bg-white rounded-l-lg">
+          <span class="text-blue-900 material-symbols-outlined">
+            password
+          </span>
+        </div>
+        <input
+          class="p-4 rounded-r-lg bg-blue-900 text-white"
+          type="password"
+          placeholder="Enter your password"
+          v-model.trim="password"
+        />
+      </div>
+
+      <div
+        class="flex items-center gap-1 text-white font-bold hover:text-blue-900"
       >
-        <div>
-          <input
-            type="email"
-            class="border-2 rounded-lg p-2 border-blue-600"
-            placeholder="Enter your email"
-            v-model.trim="email"
-          />
-        </div>
+        <RouterLink to="/registrarse">¿No tienes Cuenta? Crea una</RouterLink>
+        <span class="material-symbols-outlined"> arrow_right_alt </span>
+      </div>
 
-
-        <div class="d-flex justify-content-around ">
-          <input
-              class="border-2 rounded-lg p-2 border-blue-600 text-center"
-
-            type="password"
-
-            placeholder="Enter your password"
-            v-model.trim="password"
-          />
-        </div>
-
-        <div class="flex flex-col items-center w-full gap-3 font-bold">
-          <h4 class="hover:text-blue-800 cursor-pointer">
-            ¿No Tienes Una Cuenta?
-          </h4>
-
-          <div class="w-full flex justify-around text-white">
-            <button
-              type="submit"
-              class="uppercase bg-blue-800 px-4 py-2 rounded-lg"
-            >
-              Iniciar Sesion
-            </button>
-
-            <button class="uppercase bg-blue-800 px-4 py-2 rounded-lg">
-              <RouterLink to="/registrarse">Registrate</RouterLink>
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+      <div>
+        <button
+          type="submit"
+          class="bg-white text-blue-900 font-bold px-5 py-2 w-full rounded-lg hover:bg-blue-900 hover:text-white transition-all"
+        >
+          Iniciar Sesion
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
